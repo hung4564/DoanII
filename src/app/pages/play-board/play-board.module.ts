1a75013d9364cc7387a1bb5a966c63af351908ff
Hưng <hphamviet83@gmail.com>
2018-11-25 20:37:28 +0700

    sua vi tri

--- src/app/pages/play-board/play-board.module.ts
+++ src/app/pages/play-board/play-board.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PlayBoardComponent } from './play-board.component';
 import { ShareModule } from '@share/shares.module';
+import { ListPlayerCompComponent } from './list-player-comp/list-player-comp.component';
+import { ListCardCompComponent, CardDialog } from './list-card-comp/list-card-comp.component'
+
+import { ListMaterialCompComponent, ListMaterialDialog } from './list-material-comp/list-material-comp.component';
 
 @NgModule({
   imports: [
@@ -8,6 +12,15 @@ import { ShareModule } from '@share/shares.module';
   ],
   declarations: [
     PlayBoardComponent,
-  ]
+    ListPlayerCompComponent,
+    ListCardCompComponent,
+    ListMaterialCompComponent,
+    CardDialog,
+    ListMaterialDialog,
+  ],
+  entryComponents: [
+    CardDialog,
+    ListMaterialDialog,
+  ],
 })
 export class PlayBoardModule { }
--- src/app/shares/shares.module.ts
+++ src/app/shares/shares.module.ts
@@ -1,23 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { IconCompComponent } from './icon-comp/icon-comp.component';
-import { ListPlayerCompComponent } from './player/list-player-comp/list-player-comp.component';
-import { PlayerCompComponent } from './player/player-comp/player-comp.component';
+import { PlayerCompComponent } from './player-comp/player-comp.component';
 import { MaterialModule } from './material.module';
-import { CardCompComponent } from './card/card-comp/card-comp.component';
-import { ListCardCompComponent, CardDialog } from './card/list-card-comp/list-card-comp.component'
+import { CardCompComponent } from './card-comp/card-comp.component';
 import { FormsModule } from '@angular/forms';
-import { ListMaterialCompComponent, ListMaterialDialog } from './material/list-material-comp/list-material-comp.component';
 @NgModule({
   declarations: [
     IconCompComponent,
     PlayerCompComponent,
-    ListPlayerCompComponent,
     CardCompComponent,
-    ListCardCompComponent,
-    CardDialog,
-    ListMaterialCompComponent,
-    ListMaterialDialog,
   ],
   imports: [
     BrowserModule,
@@ -27,17 +19,10 @@ import { ListMaterialCompComponent, ListMaterialDialog } from './material/list-m
   exports: [
     IconCompComponent,
     PlayerCompComponent,
-    ListPlayerCompComponent,
     CardCompComponent,
-    ListCardCompComponent,
-    CardDialog,
-    ListMaterialCompComponent,
-    ListMaterialDialog,
-    BrowserModule
-  ],
-  entryComponents: [
-    CardDialog,
-    ListMaterialDialog,
+    BrowserModule,
+    FormsModule,
+    MaterialModule
   ],
   providers: [],
 })
